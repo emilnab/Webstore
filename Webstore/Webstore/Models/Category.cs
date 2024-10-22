6f108165ea1ebb00229ac40c956d37e6d4f84a15
Emil <emil95@hotmail.dk>
2024-10-22 08:55:24 +0200

    Category & Ware model classes

---
+++ Webstore/Webstore/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Webstore.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Name { get; set; }
+
+        public string? Description { get; set; }
+
+    }
+}
---
+++ Webstore/Webstore/Models/Ware.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Webstore.Models
+{
+    public class Ware
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public double Price { get; set; }
+
+        public byte[]? Photo { get; set; }
+
+    }
+}
